d3d6d780235a34239cb75e6ad350e578aa42675b
Ruben Deyhle <deyhle@entrecode.de>
2017-04-21 09:46:56 +0200

    always allow id attributes in xss filter

--- nunjucksEnv.js
+++ nunjucksEnv.js
@@ -9,7 +9,7 @@ const xss = require('xss');
 
 const xssWhitelist = {}; // allow style and class attributes on all tags
 Object.keys(xss.whiteList).forEach((tagName) => {
-  xssWhitelist[tagName] = xss.whiteList[tagName].concat(['style', 'class']);
+  xssWhitelist[tagName] = xss.whiteList[tagName].concat(['style', 'class', 'id']);
 });
 
 function setupNunjucksEnv(config, datamanager) {
